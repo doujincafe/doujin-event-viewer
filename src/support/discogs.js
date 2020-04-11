185180cf0465ef36a4d62d3bbba8c79ac705fd5d
Aiko Fujimoto <mimimememimi156@gmail.com>
2020-04-12 00:13:17 +0900

    Fixed date locale

--- src/support/discogs.js
+++ src/support/discogs.js
@@ -10,7 +10,8 @@ import events from '../common/events'
 const discogs = {
   url: /^(http?s:\/\/)(www\.discogs\.com\/)(.*\/release\/\d{1,9})/gi,
   date: /^(\d{1,2})(\s)([a-z]{1,3})(\s)(\d{1,4})/gi,
-  formatting: 'DD/MMM/YYYY',
+  formatting: 'dd MMM yyyy',
+  locale: 'en',
   el: ['.content a'],
   events: events
 }
