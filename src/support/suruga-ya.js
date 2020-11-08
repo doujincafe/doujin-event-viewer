5142c9a43551825b6bf17bd22cae24e0ecbccaa5
Aiko Fujimoto <aikoo_fujimotoo@yahoo.co.jp>
2020-11-09 02:59:55 +0900

    [fix] Updated date regex for surugaya
    Other dates are ignored causing being ignored by the script

--- src/support/suruga-ya.js
+++ src/support/suruga-ya.js
@@ -9,7 +9,7 @@ import events from '../common/events'
 
 const surugaya = {
   url: /^(http?s:\/\/)(www\.suruga-ya\.jp\/)(search\?|product\/detail\/\d{1,9})/gi,
-  date: /(\d{4})(-|\\|\/)(\d{1,2})(-|\\|\/)(\d{1,2})/gi,
+  date: /^\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])$/g,
   el: ['.t_contents', '.release_date'],
   events: events
 }
