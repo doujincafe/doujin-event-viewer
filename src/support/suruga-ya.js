c5e2b53b1c7aa038d474717aa38a88cb76da4787
Aiko Fujimoto <aikoo_fujimotoo@yahoo.co.jp>
2020-11-09 15:19:13 +0900

    [fix] Update surugaya url regex to account with URLs ending with A-Z characters

--- src/support/suruga-ya.js
+++ src/support/suruga-ya.js
@@ -8,8 +8,8 @@
 import events from '../common/events'
 
 const surugaya = {
-  url: /^(http?s:\/\/)(www\.suruga-ya\.jp\/)(search\?|product\/detail\/\d{1,9})/gi,
-  date: /\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])$/g,
+  url: /^(http?s:\/\/)(www\.suruga-ya\.jp)\/(search\?|product\/detail\/[A-Za-z0-9]*)/gi,
+  date: /\d{4}\/(0?[1-9]|1[012])\/(0?[1-9]|[12][0-9]|3[01])*/g,
   el: ['.t_contents', '.release_date'],
   events: events
 }
