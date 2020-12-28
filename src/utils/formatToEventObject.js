2faff963113334e73d818cacc9400719d8077512
松前緒花 <ohana.matsumae@internal.aiko.jp>
2020-12-28 21:39:40 +0900

    chore: Add utility script that converts text input to event json
    Useful especially if people wanted to import huge amount of events. However,
    in order to prevent errors people need to follow the formatting:
    
    For single date:
    <eventName>: <date> (ex: Doujin Music 01: 2020.09.11)
    
    For range:
    <eventName>: <date>-<date> (ex: Doujin Music 01: 2020.09.11-2020.09.22)
    
    Going outside the format will cause errors. Dates must be YYYY.MM.DD
    
    I don't need to write validation for this. Instead, I'll just let the
    users do fix the problems of their input on their own.

---
+++ src/utils/formatToEventObject.js
@@ -0,0 +1,43 @@
+// ============================================================================
+// Event Viewer
+// Copyright (C) 2020 Aiko Fujimoto
+//
+// Licensed under MIT
+// ============================================================================
+
+const formatToEventObject = (text) => {
+  if (typeof text !== 'string') throw new TypeError('Invalid text input.');
+
+  // Split each lines by newline. This assumes that the input's EOL is
+  // lf (unix)
+  // Also removes empty lines so that we won't gonna throw up error when
+  // we run it.
+  return text.split('\n').filter(item => item.length > 0).map((value) => {
+    // Split text by colon. This assumes the format is <name>: <date>
+    const [name, eventDate] = value.split(':');
+
+    // Remove any whitespaces in the date.
+    const date = eventDate.replace(/\s/g, '');
+
+    // If the date has dashes on it, this implies that it is a date range.
+    if (date.indexOf('-') > 0) {
+      const [from, to] = date.split('-');
+      return {
+        evt: name,
+        value: {
+          // Replace dots to forward slashes. This implies that year, month
+          // and date is separated by dots.
+          from: from.replace(/\./g, '/'),
+          to: to.replace(/\./g, '/')
+        }
+      };
+    } else {
+      return {
+        evt: name,
+        value: date.replace(/\./g, '/')
+      };
+    }
+  });
+};
+
+module.exports = formatToEventObject;
